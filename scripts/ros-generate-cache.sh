ee2a82b3730b07bc1cbc2e9913d2f350407ffb0c
Matthias Schoepfer <m.schoepfer@rethinkrobotics.com>
2023-09-22 14:11:02 +0200

    ros-generate-cache.sh: Improve error message for debug
    Signed-off-by: Matthias Schoepfer <m.schoepfer@rethinkrobotics.com>

--- scripts/ros-generate-cache.sh
+++ scripts/ros-generate-cache.sh
@@ -89,6 +89,7 @@ abort=false
 for f in $ROSDEP_YAML_BASENAMES; do
     if ! grep -q "^yaml file://$path_to_ros_rosdistro/rosdep/$f.yaml\$" /etc/ros/rosdep/sources.list.d/20-default.list; then
         echo "ABORT: /etc/ros/rosdep/sources.list.d/20-default.list doesn't point to $f.yaml in $path_to_ros_rosdistro"
+        echo "Expected: yaml file://$path_to_ros_rosdistro/rosdep/$f.yaml"
         abort=true
     fi
 done
